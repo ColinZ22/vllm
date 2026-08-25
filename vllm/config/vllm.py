04215431bb9d17d638c6144186af879362a5689a
Jiangyun Zhu <riverclouds.zhu@qq.com>
2026-08-25 14:17:12 +0800

    Default Qwen3.8 Flash Next to model runner v2 (#35)
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm/config/vllm.py
+++ vllm/config/vllm.py
@@ -76,6 +76,10 @@ DEFAULT_V2_MODEL_RUNNER_ARCHITECTURES = frozenset(
         "KimiK3ForConditionalGeneration",
         "LongcatFlashNgramForCausalLM",
         "Qwen2MoeForCausalLM",
+        "Qwen3_8FlashNextForCausalLM",
+        "Qwen3_8FlashNextForConditionalGeneration",
+        "Qwen4ExpForCausalLM",
+        "Qwen4ExpForConditionalGeneration",
     }
 )
 
